40e85a84bda18fcc313f00cd6e14e767284cfe84
Wilko Manger <wilko@dotlin.org>
2021-12-19 21:39:48 +0100

    Support Iterator
    Signed-off-by: Wilko Manger <wilko@dotlin.org>

--- compiler/src/main/kotlin/org/dotlin/compiler/backend/steps/ir2ast/DartIrBuiltIns.kt
+++ compiler/src/main/kotlin/org/dotlin/compiler/backend/steps/ir2ast/DartIrBuiltIns.kt
@@ -53,6 +53,8 @@ class DartIrBuiltIns(
 
     val identical = symbolAt<IrSimpleFunctionSymbol>("dart.core", "identical")
 
+    val iterator = symbolAt<IrClassSymbol>("dart.core", "Iterator")
+
     val dotlin = Dotlin(this)
 
     class Dotlin(builtIns: DartIrBuiltIns) {
---
+++ compiler/src/main/kotlin/org/dotlin/compiler/backend/steps/ir2ast/lower/lowerings/builtins/IteratorLowering.kt
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2021 Wilko Manger
+ *
+ * This file is part of Dotlin.
+ *
+ * Dotlin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dotlin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dotlin.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.dotlin.compiler.backend.steps.ir2ast.lower.lowerings.builtins
+
+import org.dotlin.compiler.backend.steps.ir2ast.ir.*
+import org.dotlin.compiler.backend.steps.ir2ast.lower.*
+import org.jetbrains.kotlin.backend.common.ir.addChild
+import org.jetbrains.kotlin.backend.wasm.ir2wasm.allSuperInterfaces
+import org.jetbrains.kotlin.builtins.StandardNames
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.descriptors.Modality
+import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.builders.*
+import org.jetbrains.kotlin.ir.builders.declarations.*
+import org.jetbrains.kotlin.ir.declarations.*
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.IrReturn
+import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
+import org.jetbrains.kotlin.ir.expressions.impl.IrBlockBodyImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrExpressionBodyImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.symbols.impl.IrValueParameterSymbolImpl
+import org.jetbrains.kotlin.ir.types.*
+import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
+import org.jetbrains.kotlin.ir.types.impl.makeTypeProjection
+import org.jetbrains.kotlin.ir.util.defaultType
+import org.jetbrains.kotlin.ir.util.isInterface
+import org.jetbrains.kotlin.ir.util.parentAsClass
+import org.jetbrains.kotlin.ir.util.parentClassOrNull
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.types.Variance
+
+class IteratorLowering(private val context: DartLoweringContext) : IrDeclarationTransformer {
+    override fun transform(declaration: IrDeclaration): Transformations<IrDeclaration> {
+        if (declaration !is IrClass || !declaration.isIterator()) return noChange()
+
+        declaration.apply {
+            declaration.propertyWithName("current").apply {
+                getter = null
+
+                setter = setter!!.deepCopyWith {
+                    modality = Modality.ABSTRACT
+                }.apply {
+                    body = null
+                }
+            }
+        }
+
+        return noChange()
+    }
+}
+
+class IteratorSubtypeBackingFieldsLowering(private val context: DartLoweringContext) : IrDeclarationTransformer {
+    override fun transform(declaration: IrDeclaration): Transformations<IrDeclaration> {
+        if (declaration !is IrClass ||
+            declaration.isInterface ||
+            !declaration.allSuperInterfaces().any { it.isIterator() }) {
+            return noChange()
+        }
+
+        declaration.apply {
+            declaration.propertyWithName("current").apply {
+                val property = this
+
+                backingField = context.irFactory.buildField {
+                    name = Name.identifier("current")
+                    type = property.type
+                    isFinal = false
+                    origin = IrDeclarationOrigin.PROPERTY_BACKING_FIELD
+                    visibility = DescriptorVisibilities.PRIVATE
+                }.apply {
+                    overriddenSymbols = emptyList()
+                    parent = declaration
+                    correspondingPropertySymbol = property.symbol
+                }
+
+                getter!!.body = IrExpressionBodyImpl(
+                    expression = context.buildStatement(symbol) {
+                        irGetField(
+                            receiver = irGet(declaration.thisReceiver!!),
+                            field = backingField!!
+                        )
+                    }
+                )
+
+                setter!!.apply {
+                    body = IrExpressionBodyImpl(
+                        expression = context.buildStatement(symbol) {
+                            irSetField(
+                                receiver = irGet(declaration.thisReceiver!!),
+                                field = backingField!!,
+                                value = irGet(valueParameters[0])
+                            )
+                        }
+                    )
+                }
+            }
+        }
+
+        return noChange()
+    }
+}
+
+/**
+ * All `return`s are updated in `next()` (or `previous()` of subtypes of `Iterator` to update `current`.
+ */
+class IteratorSubtypeReturnsLowering(private val context: DartLoweringContext) : IrExpressionTransformer {
+    override fun <D> transform(
+        expression: IrExpression,
+        container: D
+    ): Transformation<IrExpression>? where D : IrDeclaration, D : IrDeclarationParent {
+        if (container.parentClassOrNull?.allSuperInterfaces()?.any { it.isIterator() } != true) {
+            return noChange()
+        }
+
+        if (container !is IrSimpleFunction) return noChange()
+
+        val overrideParent = container.resolveOverride()?.parentClassOrNull ?: return noChange()
+        if (!(overrideParent.isIterator() || overrideParent.isListIterator()) ||
+            !(container.name == Name.identifier("next") || container.name == Name.identifier("previous"))
+        ) {
+            return noChange()
+        }
+
+        if (expression !is IrReturn) return noChange()
+
+        val iteratorClass = context.irBuiltIns.iteratorClass.owner
+        val property = iteratorClass.propertyWithName("current")
+
+        return replaceWith(
+            IrReturnImpl(
+                UNDEFINED_OFFSET, UNDEFINED_OFFSET,
+                type = expression.type,
+                returnTargetSymbol = expression.returnTargetSymbol,
+                value = context.buildStatement(container.symbol) {
+                    irCall(
+                        property.setter!!.symbol,
+                        type = property.type,
+                        origin = IrStatementOrigin.EQ,
+                    ).apply {
+                        dispatchReceiver = irGet(property.parentAsClass.thisReceiver!!)
+
+                        putValueArgument(
+                            index = 0,
+                            valueArgument = expression.value
+                        )
+                    }
+                }
+            )
+        )
+    }
+}
+
+
+private fun IrClass.isIterator() = defaultType.classifier.signature ==
+        getPublicSignature(StandardNames.COLLECTIONS_PACKAGE_FQ_NAME, "Iterator")
+
+private fun IrClass.isListIterator() = defaultType.classifier.signature ==
+        getPublicSignature(StandardNames.COLLECTIONS_PACKAGE_FQ_NAME, "ListIterator")
